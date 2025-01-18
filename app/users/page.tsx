e9a85e72e76d435e958a878c542cef59b6498248
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-01-18 17:09:43 +0530

    initials

---
+++ app/users/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+interface User {
+    id: number,
+    name: string
+}
+
+const page = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users: User[] = await response.json();
+  return (
+      <div>Users: 
+          <ul>
+              {users.map(user => <li key={user.id}>{user.name}</li>)}
+          </ul>
+    </div>
+  )
+}
+
+export default page;
