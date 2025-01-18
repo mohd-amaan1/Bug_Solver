4d659b115049807c85f56d07f116841347f8c876
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-01-18 17:13:33 +0530

    cache

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,9 @@ interface User {
 }
 
 const page = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+        cache: "no-store"
+    });
     const users: User[] = await response.json();
   return (
       <div>Users: 
