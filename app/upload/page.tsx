97252d4614c21147686c9e7370a4c624475f707e
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-01-23 20:34:19 +0530

    upload image

---
+++ app/upload/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+import React, { useState } from 'react'
+import { CldUploadWidget, CldImage } from 'next-cloudinary'
+
+interface CloudinaryResult {
+  public_id: string
+}
+const uploadImage = () => {
+  const [publicId, setPublicId] = useState('');
+  return (
+    <>
+      {publicId && <CldImage src={publicId} height={270} width={180} alt='Image Uploaded'/>}
+    <CldUploadWidget uploadPreset='SigmaMA' onSuccess={(result, widget) => {
+      if (result.event !== 'success') return;
+      const info = result.info as CloudinaryResult;
+        setPublicId(info.public_id);
+    }}>
+        {({open}) => <button className='btn btn-primary' onClick={() => open()}>Upload</button>}
+      </CldUploadWidget>
+      </>
+  )
+}
+
+export default uploadImage;
+
+
+
+// Asif ka dost hai Deepak Gangwar
+// Tumne kai baar dekha hoga unko baat karte hue aur haste hue
+// GAD
+// GAD
+// GAD
+// GAD
+// GAD
+// GAD
